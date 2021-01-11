901839a455bece422fb69cc517e4492729b3ad7d
onkarnirhali <43208908+onkarnirhali@users.noreply.github.com>
2021-01-11 23:24:54 +0530

    2nd checkin
    Now we support archiving chats on running of script, instead of waiting for a message form the blocklisted contact or group

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ var parse = require('csv-parse')
 wa.create().then(client => start(client));
 
 async function start(client) {
+    firstCheck(client)
     receiveNewAllMessages(client);
 }
 
 async function receiveNewAllMessages(client) {
     client.onMessage(message => {
-        console.log(message);
+       // console.log(message);
         if (message.isGroupMsg === true) { // message received from a Group
             console.log(`INFO: Group Message Received`);
             readGroups(message, client);
@@ -37,7 +38,7 @@ function readContacts(message, client) { // checks for new Personal Messages and
                 if (message.sender.formattedName === contactUser) {
                     console.log(`\n----------------------------`);
                     client.archiveChat(message.sender.id, true);
-                    console.log(`Private Message "${message.body}" was received from ${message.sender.formattedName} and has been archived`);
+                    console.log(`INFO : Private Message "${message.body}" was received from ${message.sender.formattedName} and has been archived`);
                 }
             });
         })
@@ -56,6 +57,57 @@ function readGroups(message, client) {// checks for new Group Messages and Archi
                     console.log(`\n----------------------------`);
                     client.archiveChat(message.chat.groupMetadata.id, true);
                     console.log(`INFO : Message "${message.body}" was received in Group "${message.chat.formattedTitle}" by "${message.sender.formattedName}" and has been archived`);
+                }
+            });
+        })
+}
+
+async function firstCheck(client) {
+    var allMessages = await client.getAllChats(false);
+    var allGroups = await client.getAllGroups(true);
+    allMessages.forEach(element => {
+        readContactsOnStart(element, client);
+    });
+    allGroups.forEach(element => {
+        readGroupsOnStart(element, client);
+    });
+    //console.log("All groups are =>", allGroups);
+}
+
+function readContactsOnStart(message, client) { // checks for Personal Messages on start of the script and Archives from the selected contacts
+    const data = [];
+    fs.createReadStream('contacts.csv')
+        .pipe(parse({ delimiter: ',' }))
+        .on('data', (r) => {
+            //console.log(r);
+            data.push(r);
+        })
+        .on('end', () => {
+            //console.log(data);
+            data[0].forEach(contactUser => {
+                //console.log("Names for each contact are =>", contactUser);
+                if (message.formattedTitle === contactUser) {
+                    console.log(`\n----------------------------`);
+                    client.archiveChat(message.id, true);
+                    console.log(`INFO : Private Chat "${message.formattedTitle}" has been archived`);
+                }
+            });
+        })
+}
+
+function readGroupsOnStart(message, client) {// checks for Group Messages and Archives from the selected contact
+    const data = [];
+    fs.createReadStream('groups.csv')
+        .pipe(parse({ delimiter: ',' }))
+        .on('data', (r) => {
+            data.push(r);
+        })
+        .on('end', () => {
+            data[0].forEach(groupUser => {
+                if (message.formattedTitle === groupUser) {
+                    console.log(`\n----------------------------`);
+                    client.archiveChat(message.id, true);
+                    console.log(`INFO : Group Message "${message.formattedTitle}" has been archived`);
                 }
             });
         })
